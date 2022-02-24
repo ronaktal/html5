f2357317943c10317920ba01adb9636233ce4345
ronaktal <ronaktal@gmail.com>
2022-02-24 11:28:29 -0500

    updated code

---
+++ app.js
@@ -0,0 +1,48 @@
+var app = angular.module('plunker', ['ngAnimate']);
+
+app.controller('MainCtrl', function($scope, $http, $timeout) {
+
+  var loadTime = 1000, //Load the data every second
+    errorCount = 0, //Counter for the server errors
+    loadPromise; //Pointer to the promise created by the Angular $timout service
+
+  var getData = function() {
+    $http.get('http://httpbin.org/delay/1?now=' + Date.now())
+
+    .then(function(res) {
+      $scope.data = res.data.args;
+
+      errorCount = 0;
+      nextLoad();
+    })
+
+    .catch(function(res) {
+      $scope.data = 'Server error';
+      nextLoad(++errorCount * 2 * loadTime);
+    });
+  };
+
+  var cancelNextLoad = function() {
+    $timeout.cancel(loadPromise);
+  };
+
+  var nextLoad = function(mill) {
+    mill = mill || loadTime;
+    
+    //Always make sure the last timeout is cleared before starting a new one
+    cancelNextLoad();
+    loadPromise = $timeout(getData, mill);
+  };
+
+
+  //Start polling the data from the server
+  getData();
+
+
+  //Always clear the timeout when the view is destroyed, otherwise it will keep polling
+  $scope.$on('$destroy', function() {
+    cancelNextLoad();
+  });
+
+  $scope.data = 'Loading...';
+});
